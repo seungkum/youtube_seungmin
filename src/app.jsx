0745c54d59b2bf20bc29deeb8b39483bfbc7ff52
baek.seungmin <83046110+seungkum@users.noreply.github.com>
2022-02-06 16:38:52 +0900

    #20220206-2

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect,useState } from 'react';
 import "./app.css";
+import VideoList from './components/video_list/video_list';
 
 function App() {
-  return (
-   <h1>Hello :)</h1>
-  );
+  const [videos, setVideos] = useState([]);  
+
+  useEffect(()=>{
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+    
+    fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyABaWZIfeIXplHfSj9o3-aez7LFxmLTybI", requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.items)) //컴포넌트 데이트를 어벧이트 해준다.
+      .catch(error => console.log('error', error));
+
+    console.log('useEffect');
+  }, []); 
+  return <VideoList videos={videos} />
 }
 
 export default App;
