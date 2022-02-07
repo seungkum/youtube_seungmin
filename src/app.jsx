f7e8bf66a4abfe47b43215cdab670aae29476d13
baek.seungmin <83046110+seungkum@users.noreply.github.com>
2022-02-07 09:04:25 +0900

    postcss styling

--- src/app.jsx
+++ src/app.jsx
@@ -1,24 +1,33 @@
-import React, { useEffect,useState } from 'react';
-import "./app.css";
-import VideoList from './components/video_list/video_list';
+import React, { useEffect, useState } from "react";
+import styles from './app.module.css'
+import SearchHeader from "./components/search_header/search_header";
+import VideoList from "./components/video_list/video_list";
 
 function App() {
-  const [videos, setVideos] = useState([]);  
+  const [videos, setVideos] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     const requestOptions = {
-      method: 'GET',
-      redirect: 'follow',
+      method: "GET",
+      redirect: "follow",
     };
-    
-    fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyABaWZIfeIXplHfSj9o3-aez7LFxmLTybI", requestOptions)
-      .then(response => response.json())
-      .then(result => setVideos(result.items)) //컴포넌트 데이트를 어벧이트 해준다.
-      .catch(error => console.log('error', error));
 
-    console.log('useEffect');
-  }, []); 
-  return <VideoList videos={videos} />
+    fetch(
+      "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyABaWZIfeIXplHfSj9o3-aez7LFxmLTybI",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items)) //컴포넌트 데이트를 어벧이트 해준다.
+      .catch((error) => console.log("error", error));
+
+    console.log("useEffect");
+  }, []);
+  return (
+    <div className={styles.app}>
+      <SearchHeader />
+      <VideoList videos={videos} />
+    </div>
+  );
 }
 
 export default App;
