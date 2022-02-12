de6914ec1149f49d57fcec8bfc0874a2e34cffaf
baek.seungmin <83046110+seungkum@users.noreply.github.com>
2022-02-12 22:34:09 +0900

    클릭시상세화면구현-1

---
+++ src/components/video_detail/video_detail.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './video_detail.module.css';
+
+const VideoDetail = ({video}) => (
+    <h1>{video.snippet.title}</h1>
+)
+            
+    
+
+export default VideoDetail;
