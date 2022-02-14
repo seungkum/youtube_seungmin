79c33dd8a8d749b06b7eb77ad3d1ec319b94cf32
baek.seungmin <83046110+seungkum@users.noreply.github.com>
2022-02-14 11:17:49 +0900

    유튜브 재정리

--- src/components/search_header/search_header.jsx
+++ src/components/search_header/search_header.jsx
@@ -1,35 +1,39 @@
-import React, { useRef } from 'react';
+import React, { memo, useRef } from 'react';
 import styles from './search_header.module.css'
 
-const SearchHeader = ({onSearch}) => { //검색이라는 이벤트가 발생하면 내가 전달해주는 이 콜백함수를 불러
-    const inputRef = useRef();
-    const handleSearch=()=>{
-        const value = inputRef.current.value;
-        console.log(value)
-        onSearch(value); //검색하는것을 prop으로 받아와야한다.
-
-    }
-    const onClick =()=>{
-        handleSearch();
-        // console.log('onClick')
-    }
-    const onKeyPress =(event)=>{
-        if(event.key ==='Enter'){handleSearch();}
-        console.log('onKeyPress')
-
-    }
-     return (
-         <header className={styles.header}>
-             <div className={styles.logo}>
-             <img className={styles.img} src="/images/logo.png" alt="logo" />
-             <h1 className={styles.title}>Youtube</h1>
-             </div>
-             <input ref={inputRef} className={styles.input} type="search" placeholder='Search..' onKeyPress={onKeyPress}/>
-             <button className={styles.button} type='submit'>
-                 <img className={styles.buttonImg} src="/images/search.png" alt="search" onClick={onClick}/>
-             </button>
-         </header>
-     )
-    };
+const SearchHeader = memo(
+    ({onSearch}) => { //검색이라는 이벤트가 발생하면 내가 전달해주는 이 콜백함수를 불러
+        const inputRef = useRef();
+        const handleSearch=()=>{
+            const value = inputRef.current.value;
+            console.log(value)
+            onSearch(value); //검색하는것을 prop으로 받아와야한다.
+    
+        }
+        const onClick =()=>{
+            handleSearch();
+            // console.log('onClick')
+        }
+        const onKeyPress =(event)=>{
+            if(event.key ==='Enter'){handleSearch();}
+            console.log('onKeyPress')
+    
+        }
+         return (
+             <header className={styles.header}>
+                 <div className={styles.logo}>
+                 <img className={styles.img} src="/images/logo.png" alt="logo" />
+                 <h1 className={styles.title}>Youtube</h1>
+                 </div>
+                 <input ref={inputRef} className={styles.input} type="search" placeholder='Search..' onKeyPress={onKeyPress}/>
+                 <button className={styles.button} type='submit'>
+                     <img className={styles.buttonImg} src="/images/search.png" alt="search" onClick={onClick}/>
+                 </button>
+             </header>
+         )
+        }
+);
+// memo를 쓴다는것은 전달되는 props이 변경되지 않으면 re-render가 되지않고 prop이 바뀌면 다시 렌더링이된다.
+//그말은 onSearch가 새로운것으로 업데이트가되면 searchHeader 렌더가 발생할수있다.
 
 export default SearchHeader;
--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,24 +1,26 @@
-import React from 'react';
+import React, { memo } from 'react';
 import styles from './video_item.module.css';
 
-const VideoItem = ({ video, video: { snippet }, onVideoClick, display }) => {
-  const displayType = display ==='list'? styles.list : styles.grid;
-
-  return(
-  <li className={`${styles.container} ${displayType}`} onClick={()=> onVideoClick(video)}>
-    <div className={styles.video}>
-      <img
-        className={styles.thumbnail}
-        src={snippet.thumbnails.medium.url}
-        alt="video thumbnail"
-      />
-      <div className={styles.metadata}>
-        <p className={styles.title}>{snippet.title}</p>
-        <p className={styles.channel}>{snippet.channelTitle}</p>
+const VideoItem = memo(
+  ({ video, video: { snippet }, onVideoClick, display }) => {
+    const displayType = display ==='list'? styles.list : styles.grid;
+  
+    return(
+    <li className={`${styles.container} ${displayType}`} onClick={()=> onVideoClick(video)}>
+      <div className={styles.video}>
+        <img
+          className={styles.thumbnail}
+          src={snippet.thumbnails.medium.url}
+          alt="video thumbnail"
+        />
+        <div className={styles.metadata}>
+          <p className={styles.title}>{snippet.title}</p>
+          <p className={styles.channel}>{snippet.channelTitle}</p>
+        </div>
       </div>
-    </div>
-  </li>
-  )
-};
+    </li>
+    )
+  }
+); //prop이 바뀌지 않고 업데이트가 될 필요가 없기때문에 memo사용
 
 export default VideoItem;
